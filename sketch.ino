8211d50a1b9c3c09659252d9dfb8d45a0a0f9b1f
Gerardo Garcia Lopez <gerardoagl96@gmail.com>
2024-08-06 17:55:46 -0500

    Bluetooth sync

--- sketch.ino
+++ sketch.ino
@@ -1,16 +1,27 @@
 #include <SPI.h>
 #include <TMRpcm.h>
-
-TMRpcm audio;
+#include <SoftwareSerial.h>
 
 const int INPUT_PIN = A0;
 const int BUTTON_PIN = 7;
 const int SD_CS_PIN = 4;
+const char SYNC_KEYWORD[] = "SYNC";
+const byte RX_PIN = 3;
+const byte TX_PIN = 5;
 
 bool is_recording = false;
+byte buff[100];
+
+File root;
+File tracker;
+int track;
+
+TMRpcm audio;
+SoftwareSerial BT(TX_PIN, RX_PIN);
 
 void setup() {
   Serial.begin(9600);
+  BT.begin(9600);
 
   pinMode(INPUT_PIN, INPUT);
   pinMode(BUTTON_PIN, INPUT);
@@ -23,6 +34,23 @@ void setup() {
     return; 
   }
 
+  byte chunk;
+
+  if (SD.exists("tracker.txt")) {
+    tracker = SD.open("tracker.txt");
+    String read = "";
+    
+    while (tracker.available()) {
+      read += char(tracker.read());
+    }
+
+    track = read.toInt();
+
+  } else {
+    track = 4386;
+  }
+
+
   audio.CSPin = SD_CS_PIN;
 }
 
@@ -32,14 +60,110 @@ void loop() {
   if (record == HIGH) {
     if (is_recording == false) {
       Serial.println("Recording...");
-      audio.startRecording("test.wav", 16000, INPUT_PIN);
+      
       is_recording = true;
+
+      track = track + 1;
+      
+      audio.startRecording(track + ".wav", 16000, INPUT_PIN);
     }
   } else {
     if (is_recording) {
       Serial.println("Stop");
-      audio.stopRecording("test.wav");
+      
+      audio.stopRecording(track + ".wav");
+      
       is_recording = false;
+      
+      SD.remove("tracker.txt");
+      
+      tracker = SD.open("tracker.txt", FILE_WRITE);
+      tracker.print(track);
+      tracker.close();
+
+      Serial.println(track);
     }
   }
+
+  if (BT.available()) {
+    String message = getMessage();
+
+    if (message == SYNC_KEYWORD) {
+      Serial.println("Let's Sync!");
+
+      root = SD.open("/");
+      File entry = root.openNextFile();
+      int quantity = 0;
+      
+      while (entry) {
+        String name = entry.name();
+
+        if (
+          entry.isDirectory() == false &&
+          name.endsWith(".WAV")
+        ) {
+          quantity += 1;          
+        }
+
+        entry.close();
+
+        entry = root.openNextFile();
+      }
+
+      BT.print(quantity);
+      BT.print("@");
+
+      root.close();
+
+      root = SD.open("/");
+
+      entry = root.openNextFile();
+      
+      while (entry) {
+        String name = entry.name();
+
+        if (
+          entry.isDirectory() == false &&
+          name.endsWith(".WAV")
+        ) {
+
+          BT.print(entry.name());
+          BT.print(",");
+          BT.print(entry.size());
+          BT.print("@");
+
+          while (entry.available()) {
+            int bytes = entry.read(buff, 100);
+
+            BT.write(buff, bytes);
+          }
+        }
+
+        entry.close();
+
+        entry = root.openNextFile();
+      }
+
+      root.close();
+
+    } else {
+      Serial.print(message);
+    }
+  }
+}
+
+String getMessage () {
+  String msg = "";
+
+  while (BT.available()) {
+    char c = (char) BT.read();
+
+    msg += c;
+
+    delay(1);
+  }
+
+  msg.trim();
+
+  return msg;
 }
